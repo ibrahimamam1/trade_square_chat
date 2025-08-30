991a7b83963d12b1a02480d83160cce8d3db80c9
ibrahimamam1 <ibrahima59@iut-dhaka.edu>
2025-08-30 16:28:01 +0600

    Initial Commit

---
+++ src/main/java/iut/cse/trade_square_chat/TradeSquareChatApplication.java
@@ -0,0 +1,13 @@
+package iut.cse.trade_square_chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeSquareChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TradeSquareChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/iut/cse/trade_square_chat/TradeSquareChatApplicationTests.java
@@ -0,0 +1,13 @@
+package iut.cse.trade_square_chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeSquareChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
