e18f5b52e82460bf1febc5d5ca341f9812d9ca5c
ibrahimamam1 <ibrahima59@iut-dhaka.edu>
2025-09-24 19:27:01 +0600

    Web Socket Connect and Message

---
+++ src/main/java/iut/cse/trade_square_chat/controllers/ChatController.java
@@ -0,0 +1,16 @@
+package iut.cse.trade_square_chat.controllers;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+    @MessageMapping("/chat")
+    @SendTo("/topic/messages")
+    public String sendMessage(@Payload String message) {
+        System.out.println("Received" + message);
+        return message;
+    }
+}
